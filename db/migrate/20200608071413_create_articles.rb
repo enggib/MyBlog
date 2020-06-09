ba2fcac7ce2d8e972f5b1a428073914ca31cb173
Ibrahim Khan <khan.mdibrahim@gmail.com>
2020-06-09 17:27:20 +0530

    My blog update with CRUD

---
+++ db/migrate/20200608071413_create_articles.rb
@@ -0,0 +1,11 @@
+class CreateArticles < ActiveRecord::Migration[6.0]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :description
+      t.timestamps
+      add_column :articles, :created_at, :datetime
+      add_column :articles, :updated_at, :datetime
+    end
+  end
+end
